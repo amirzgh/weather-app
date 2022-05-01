dd9a8ae1fd104c4f8221a413332916237e660460
royadaneshi <royadaneshi2001@gmail.com>
2022-05-01 15:55:40 +0430

    get WeatherInfo of city from openWeatherMap

--- Weather app/app/src/main/java/com/example/weatherforecast/MainActivity.java
+++ Weather app/app/src/main/java/com/example/weatherforecast/MainActivity.java
@@ -9,6 +9,9 @@ import android.os.Handler;
 import android.os.Message;
 import android.widget.Toast;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
@@ -16,6 +19,11 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        ////
+        double g = 40.730610;
+        double s = -73.935242;
+        WeatherInfo.getWeatherInfoByCoordinates(g, s, getApplicationContext(), new WeatherInfoHandler());
+////
         GeoLocation geoLocation = new GeoLocation();
         geoLocation.getAddress("a", getApplicationContext(), new GeoHandler());
     }
@@ -24,7 +32,7 @@ public class MainActivity extends AppCompatActivity {
         @Override
         public void handleMessage(@NonNull Message msg) {
             String address;
-            switch (msg.what){
+            switch (msg.what) {
                 case 1:
                     Bundle bundle = msg.getData();
                     address = bundle.getString("address");
@@ -32,9 +40,25 @@ public class MainActivity extends AppCompatActivity {
                 default:
                     address = null;
             }
-            if(address != null) {
+            if (address != null) {
                 Toast.makeText(getApplicationContext(), address, Toast.LENGTH_SHORT).show();
             }
+        }
+    }
+
+    public static class WeatherInfoHandler extends Handler {
+        public static ArrayList<String> weather = new ArrayList<>();
+
+        @Override
+        public void handleMessage(@NonNull Message msg) {
+
+            Bundle bundle = msg.getData();
+            weather.clear();
+            weather = bundle.getStringArrayList("cityWeatherInfo");
+//            for (int i = 0; i < 6; i++) {
+//                System.out.println(weather.get(i) + " ----------------------------------------------------------------\n\n\n");
+//
+//            }
         }
     }
 }
