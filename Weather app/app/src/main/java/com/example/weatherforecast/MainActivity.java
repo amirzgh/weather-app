5ed9cb06aafbb096d74bc51c8c5dbafba9450808
royadaneshi <royadaneshi2001@gmail.com>
2022-05-01 18:39:17 +0430

    add check connectivity to internet

--- Weather app/app/src/main/java/com/example/weatherforecast/MainActivity.java
+++ Weather app/app/src/main/java/com/example/weatherforecast/MainActivity.java
@@ -2,7 +2,6 @@ package com.example.weatherforecast;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.Message;
