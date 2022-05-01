e970e8806b967c4801e20b96d86ec56b59562ec1
royadaneshi <royadaneshi2001@gmail.com>
2022-05-01 17:21:21 +0430

    delete thread from get WeatherInfo of city from openWeatherMap

--- Weather app/app/src/main/java/com/example/weatherforecast/MainActivity.java
+++ Weather app/app/src/main/java/com/example/weatherforecast/MainActivity.java
@@ -3,14 +3,13 @@ package com.example.weatherforecast;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.annotation.SuppressLint;
+
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.Message;
 import android.widget.Toast;
 
 import java.util.ArrayList;
-import java.util.HashMap;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -19,14 +18,11 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        ////
-        double g = 40.730610;
-        double s = -73.935242;
-        WeatherInfo.getWeatherInfoByCoordinates(g, s, getApplicationContext(), new WeatherInfoHandler());
-////
         GeoLocation geoLocation = new GeoLocation();
         geoLocation.getAddress("a", getApplicationContext(), new GeoHandler());
     }
+
+
 
     private class GeoHandler extends Handler {
         @Override
@@ -46,18 +42,7 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
-    public static class WeatherInfoHandler extends Handler {
-        public static ArrayList<String> weather = new ArrayList<>();
 
-        @Override
-        public void handleMessage(@NonNull Message msg) {
-            Bundle bundle = msg.getData();
-            weather.clear();
-            weather = bundle.getStringArrayList("cityWeatherInfo");
-//            for (int i = 0; i < 6; i++) {
-//                System.out.println(weather.get(i) + " ----------------------------------------------------------------\n\n\n");
-//
-//            }
-        }
-    }
+
+
 }
